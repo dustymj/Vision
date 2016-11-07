24350aaa1b9ddbe100f8299833c3dec0e4cf8d82
dustymj <dustymj@msn.com>
2016-11-06 22:15:31 -0600

    The basics
    The basic structure of the main class and preprocess class.
    
    Ideally, a third class will come along which will handle the
    detection of the box. For all I care this can be called the vision
    class.
    
    The main class will load in the desired image, and then call functions
    from the classes to perform operations on the image.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdlib>
+#include "preprocess.cpp"
+
+int main() {
+
+   /* Main outline for the program.
+
+   1. Load in the image, declare all variables as necessary.
+
+   2. Perform preprocessing.
+      a. Historgram equalization
+      b. Increase contrast
+      c. Find zeroes across laplacian
+      d. Reduce noise throughout
+
+   3. Find the box
+      a. Hough Transformation
+   */
+}
---
+++ preprocess.cpp
@@ -0,0 +1,4 @@
+// preprocess.cpp
+// This will be used to define various functions to perform preprocessing on
+// an image before moving forward to detect a box. 
+
