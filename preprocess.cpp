cede771b853f50cb9a172d970638cc1057e23cac
Micah Most <micahmost12@gmail.com>
2016-11-09 19:16:04 -0600

    Modified main.cpp, to run with the functions located in image. Modified some variable names in image.cpp to follow our standard. Added two functions in preprocess.cpp: Change_Brightness and Change_Contrast. Added sub directory containing the images.

--- preprocess.cpp
+++ preprocess.cpp
@@ -1,4 +1,73 @@
 // preprocess.cpp
 // This will be used to define various functions to perform preprocessing on
-// an image before moving forward to detect a box. 
+// an image before moving forward to detect a box.
 
+/*------------------------------------------------------------
+   Change_Brightness
+
+   INPUTS
+   image - Pointer to a bitmap image
+   int   - Level of brightness change
+
+   DESCRIPTION
+   Changes the brightness of the image
+
+   RETURNS
+   Nothing
+-------------------------------------------------------------*/
+void Change_Brightness(bmpBITMAP_FILE &image, int level) {
+
+   int bitmap_width;
+   int bitmap_height;
+   int reduction;
+
+   bitmap_height = Assemble_Integer(image.info_header.biHeight);
+   bitmap_width  = Assemble_Integer(image.info_header.biWidth);
+
+   for (int i = 0; i < bitmap_height; i++) {
+      for (int j = 0; j < bitmap_width; j++) {
+         reduction = int(image.image_ptr[i][j]) + level;
+
+         if(reduction < 0)
+            image.image_ptr[i][j] = 0;
+         else
+            image.image_ptr[i][j] = reduction;
+      }
+   }
+}
+
+/*------------------------------------------------------------
+   Change_Contrast
+
+   INPUTS
+   image - Pointer to a bitmap image
+   float - Level of contrast change
+
+   DESCRIPTION
+   Changes the contrast in the image.
+
+   RETURNS
+   Nothing
+-------------------------------------------------------------*/
+void Change_Contrast(bmpBITMAP_FILE &image, int level) {
+
+   int bitmap_width;
+   int bitmap_height;
+   int change_level;
+
+   bitmap_height = Assemble_Integer(image.info_header.biHeight);
+   bitmap_width  = Assemble_Integer(image.info_header.biWidth);
+
+   for (int i = 0; i < bitmap_height; i++) {
+      for (int j = 0; j < bitmap_width; j++) {
+         change_level = 127 + level * (int(image.image_ptr[i][j]) - 127);
+
+         if(change_level < 0)
+            image.image_ptr[i][j] = 0;
+         else if(change_level > 255)
+            image.image_ptr[i][j] = 255;
+         else
+            image.image_ptr[i][j] = change_level;
+      }
+   }
+}
